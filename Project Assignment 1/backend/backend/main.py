314a0a38eab1e38600f286b97912054d071a5919
Aditya Shrivastava <adityashrivastava2005@gmail.com>
2026-03-19 21:41:56 +0530

    restructured

--- Project Assignment 1/backend/backend/main.py
+++ Project Assignment 1/backend/backend/main.py
@@ -12,7 +12,7 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-class User(Base):
+class User(Base):   
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
